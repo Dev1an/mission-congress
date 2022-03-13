cdce5f5817eba955de5314a27f08f9d2e10bb1a7
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2022-03-13 17:56:17 +0100

    Add detailed schedule

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -21,16 +21,16 @@
 	</div>
 
 	<div class="schedule">
-		<a href="https://google.com">
-			<div>19:00 <span class=location>- Basiliek van Koekelberg</span></div>
+		<a href="/schedule">
+			<div>19:00 <span class=secondary>- Basiliek van Koekelberg</span></div>
 			<h4>Welkom en registratie</h4>
 		</a>
-		<a href="https://google.com">
-			<div>20:00 <span class=location>- Basiliek van Koekelberg</span></div>
+		<a href="/schedule">
+			<div>20:00 <span class=secondary>- Basiliek van Koekelberg</span></div>
 			<h4>Openingsavond van het missiecongres</h4>
 		</a>
-		<a href="https://google.com">
-			<div>22:30 <span class=location>- School: Notre-Dame de la Sagesse</span></div>
+		<a href="/schedule">
+			<div>22:30 <span class=secondary>- School: Notre-Dame de la Sagesse</span></div>
 			<h4>Bar</h4>
 		</a>
 	</div>
@@ -68,9 +68,5 @@
 		.program-overview .schedule a:not(:last-child) {
 			border-bottom: 0.5px solid rgba(0, 0, 0, 0.233);
 		}
-	}
-
-	.program-overview .schedule a .location {
-		color: rgba(0, 0, 0, 0.45);
 	}
 </style>
---
+++ svelte/src/routes/schedule.svelte
@@ -0,0 +1,115 @@
+<script lang="ts">
+	import NavigationBar from "$lib/components/Navigation bar.svelte";
+</script>
+
+<NavigationBar />
+
+<div class="search-row">
+	<input type="text" placeholder="Search">
+</div>
+
+<div class="schedule">
+	<div class="time-indicator">19:00 <span class=day>- Vrijdag</span></div>
+	<a href="https://google.com">
+		<p class="title">Comment mettre sa créativité artistique au service de l'Annonce de l'Evangile ?</p>
+		<p class="secondary">Culture • Workshop • 50 min</p>
+	</a>
+	<a href="https://google.com">
+		<p class="title">De Kerk in een geseculariseerde samenleving</p>
+		<p class="secondary">Panelgesprek • 50 min • Basiliek van Koekelberg</p>
+	</a>
+	<a href="https://google.com">
+		<p>Hello schedule!</p>
+	</a>
+	<a href="https://google.com">
+		<p>Hello schedule!</p>
+	</a>
+</div>
+
+<style>
+	.search-row {
+		padding: 0 var(--horizontal-safe-area) 0.7em;
+		padding-left:  max(var(--horizontal-safe-area), env(safe-area-inset-left));
+		padding-right: max(var(--horizontal-safe-area), env(safe-area-inset-right));
+
+		display: flex;
+	}
+
+	input {
+		flex-grow: 1;
+		padding: 0.4em 0.6em;
+
+		background-color: rgba(0, 0, 0, 0.08);
+		border-radius: 8px;
+	}
+
+	input::placeholder {
+		color:rgba(0, 0, 0, 0.4);
+	}
+
+	.schedule {
+		position: relative;
+		font-size: 18px;
+	}
+
+	.time-indicator {
+		position: -webkit-sticky;
+		position: sticky;
+		top: 0;
+		padding: 0.3em var(--horizontal-safe-area);
+		padding-left:  max(var(--horizontal-safe-area), env(safe-area-inset-left));
+		padding-right: max(var(--horizontal-safe-area), env(safe-area-inset-right));
+
+		font-weight: bold;
+
+		color: rgba(0, 0, 0, 0.644);
+		background-color: #FAFAFA;
+	}
+
+	@supports ( -webkit-backdrop-filter: blur(10px) ) or ( backdrop-filter: blur(10px) ) {
+		.time-indicator {
+			background-color: rgba(243, 243, 243, 0.5);
+		}
+	}
+	@supports ( -webkit-backdrop-filter: blur(20px) ) {
+		.time-indicator {
+			-webkit-backdrop-filter: blur(20px);
+		}
+	}
+	@supports ( backdrop-filter: blur(20px) ) {
+		.time-indicator {
+			backdrop-filter: blur(20px)
+		}
+	}
+
+	.time-indicator .day {
+		font-weight: normal;
+	}
+
+	.schedule a {
+		display: block;
+		padding: 0.6em 0;
+		margin-left:  var(--horizontal-safe-area);
+		margin-left:  max(var(--horizontal-safe-area), env(safe-area-inset-left));
+		padding-right: var(--horizontal-safe-area);
+		padding-right: max(var(--horizontal-safe-area), env(safe-area-inset-right));
+	}
+
+	.schedule a p.title {
+		margin-bottom: 0.2em;
+	}
+
+	.schedule a .secondary {
+		font-size: 16px;
+	}
+
+	.schedule a:not(:last-child) {
+		border-bottom: 1px solid rgba(0, 0, 0, 0.233);
+	}
+
+	@media (-webkit-min-device-pixel-ratio: 2) {
+		.schedule a:not(:last-child) {
+			border-bottom: 0.5px solid rgba(0, 0, 0, 0.233);
+		}
+	}
+</style>
