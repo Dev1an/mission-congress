e5a91dabd46b2d12d98ee6c0a64ae564d439b91e
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2022-03-13 10:49:36 +0100

    Add separator lines

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -2,7 +2,7 @@
 	import sloganNl from '$lib/assets/images/slogan/nl.jpg';
 	import SegmentedPicker from '$lib/components/segmented picker/main.svelte'
 
-	let selectedDay = 'Zat'
+	let selectedDay = 'Vr'
 </script>
 
 <nav>
@@ -23,16 +23,15 @@
 
 	<div class="schedule">
 		<article>
-			<div>19:00</div>
+			<div>19:00 <span class=location>- Basiliek van Koekelberg</span></div>
 			<h4>Welkom en registratie</h4>
 		</article>
 		<article>
-			<div>20:00</div>
+			<div>20:00 <span class=location>- Basiliek van Koekelberg</span></div>
 			<h4>Openingsavond van het missiecongres</h4>
-			<p>Gebed, lectio divina en aanbidding in aanwezigheid van bisschop Kockerols</p>
 		</article>
 		<article>
-			<div>22:30</div>
+			<div>22:30 <span class=location>- School: Notre-Dame de la Sagesse</span></div>
 			<h4>Bar</h4>
 		</article>
 	</div>
@@ -41,28 +40,43 @@
 <style>
 	nav {
 		padding: 0.7em var(--horizontal-safe-area);
+		padding-left: max(var(--horizontal-safe-area), env(safe-area-inset-left));
+		padding-right: max(var(--horizontal-safe-area), env(safe-area-inset-right));
 	}
 
 	section.slogan {
 		text-align: center;
 		padding: 3em var(--horizontal-safe-area);
+		padding-left: max(var(--horizontal-safe-area), env(safe-area-inset-left));
+		padding-right: max(var(--horizontal-safe-area), env(safe-area-inset-right));
 		background-color: #f79f1b;
 	}
 
 	.day-picker {
 		padding: 0 var(--horizontal-safe-area);
-	}
-
-	.program-overview .schedule {
-		margin: 0 var(--horizontal-safe-area);
-		margin-top: 1.2em;
+		padding-left:  max(var(--horizontal-safe-area), env(safe-area-inset-left));
+		padding-right: max(var(--horizontal-safe-area), env(safe-area-inset-right));
 	}
 
 	.program-overview .schedule article {
-		margin-bottom: 1.2em;
+		padding: 0.6em 0;
+		margin-left:  var(--horizontal-safe-area);
+		margin-left:  max(var(--horizontal-safe-area), env(safe-area-inset-left));
+		padding-right: var(--horizontal-safe-area);
+		padding-right: max(var(--horizontal-safe-area), env(safe-area-inset-right));
 	}
 
-	.program-overview .schedule article p {
-		margin-top: 0.2em;
+	.program-overview .schedule article:not(:last-child) {
+		border-bottom: 1px solid rgba(0, 0, 0, 0.233);
+	}
+
+	@media (-webkit-min-device-pixel-ratio: 2) {
+		.program-overview .schedule article:not(:last-child) {
+			border-bottom: 0.5px solid rgba(0, 0, 0, 0.233);
+		}
+	}
+
+	.program-overview .schedule article .location {
+		color: rgba(0, 0, 0, 0.45);
 	}
 </style>
