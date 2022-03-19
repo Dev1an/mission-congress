63d8d44868d09bce912de5444d7f92680396f701
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2022-03-19 11:27:59 +0100

    Add link to rest of website

--- svelte/src/lib/components/Navigation bar.svelte
+++ svelte/src/lib/components/Navigation bar.svelte
@@ -1,18 +1,28 @@
 <script lang="ts">
-import { locales } from "$lib/content/locale";
+	import { locales } from "$lib/content/locale";
 
 	import type { LOCALE_CODE } from "$lib/content/schema"
 	export let language: LOCALE_CODE
 
 	const title = language == locales.french ? 'Congres Mission' : 'Missiecongres'
+	const website = language == locales.french ? 'https://www.congresmission.be' : 'https://www.missiecongres.be'
 </script>
 
 <nav>
-	<h3>{title}</h3>
+	<h3><a href={website}>{title}</a></h3>
 </nav>
 
 <style>
+	a:any-link {
+		text-decoration: none;
+	}
+
 	nav {
+		display: flex;
+		align-items: center;
+		justify-content:space-between;
+		gap: 10px;
+
 		padding: 0.7em var(--horizontal-safe-area);
 		padding-left: max(var(--horizontal-safe-area), env(safe-area-inset-left));
 		padding-right: max(var(--horizontal-safe-area), env(safe-area-inset-right));
